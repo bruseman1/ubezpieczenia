86c48608f73e9dc027ab97bbc391e29d8f47b3e5
marcinkowski-automation <marcinkowski.fotografia@gmail.com>
2025-03-16 18:44:49 +0100

    responsive menu - hamburger

--- script.js
+++ script.js
@@ -24,3 +24,11 @@ document.querySelectorAll(".toggleButton").forEach(button => {
         }
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("nav-active");
+    });
+});
